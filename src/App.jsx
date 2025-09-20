5359a37d61ed32f2752fe6c9d51f0a9394438f62
Nilanjan Guha <nilanjanguha8@gmail.com>
2025-09-20 20:40:00 +0530

    first chnage in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
   return (
     <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Hello Nilanjan</h1>
+      <h1 className="text-3xl text-indigo-300">
+        Hello Nilanjan welcome to the gsap{" "}
+      </h1>
     </div>
   );
 };
